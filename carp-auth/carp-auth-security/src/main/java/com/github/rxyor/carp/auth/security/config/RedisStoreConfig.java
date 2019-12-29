0f25c5b3aa71601ab61d5177f9f4e97208625c8c
刘阳 <liuyang03@souche.com>
2019-12-29 23:53:26 +0800

    @chore:添加redis store

---
+++ carp-auth/carp-auth-security/src/main/java/com/github/rxyor/carp/auth/security/config/RedisStoreConfig.java
@@ -0,0 +1,39 @@
+package com.github.rxyor.carp.auth.security.config;
+
+import com.github.rxyor.carp.auth.security.support.redis.FastjsonRedisTokenStoreSerializationStrategy;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+/**
+ *<p>
+ *
+ *</p>
+ *
+ * @author liuyang
+ * @date 2019/12/29 周日 23:12:00
+ * @since 1.0.0
+ */
+@AllArgsConstructor
+@Configuration
+public class RedisStoreConfig {
+
+    @Value("${spring.application.name:carp}")
+    private final String appName;
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Bean
+    @Primary
+    public RedisTokenStore redisTokenStore() {
+        RedisTokenStore store = new RedisTokenStore(redisConnectionFactory);
+        store.setPrefix(appName + "::" + "auth:");
+        store.setSerializationStrategy(new FastjsonRedisTokenStoreSerializationStrategy());
+        return store;
+    }
+
+}
---
+++ carp-auth/carp-auth-security/src/main/java/com/github/rxyor/carp/auth/security/support/redis/FastjsonRedisTokenStoreSerializationStrategy.java
@@ -0,0 +1,78 @@
+package com.github.rxyor.carp.auth.security.support.redis;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.util.IOUtils;
+import com.google.common.base.Preconditions;
+import java.nio.charset.Charset;
+import org.springframework.data.redis.serializer.SerializationException;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStoreSerializationStrategy;
+
+/**
+ *<p>
+ *
+ *</p>
+ *
+ * @author liuyang
+ * @date 2019/12/29 周日 23:18:00
+ * @since 1.0.0
+ */
+public class FastjsonRedisTokenStoreSerializationStrategy implements RedisTokenStoreSerializationStrategy {
+
+    private final static ParserConfig defaultRedisConfig = new ParserConfig();
+
+    static {
+        defaultRedisConfig.setAutoTypeSupport(true);
+    }
+
+    static {
+        //设置Fastjson Json自动转换为Java对象
+        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+    }
+
+    @Override
+    public <T> T deserialize(byte[] bytes, Class<T> clazz) {
+        Preconditions.checkArgument(clazz != null,
+            "clazz can't be null");
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+
+        try {
+            return JSON.parseObject(new String(bytes, IOUtils.UTF8), clazz, defaultRedisConfig);
+        } catch (Exception ex) {
+            throw new SerializationException("Could not serialize: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public String deserializeString(byte[] bytes) {
+        if (bytes == null || bytes.length == 0) {
+            return null;
+        }
+        return new String(bytes, IOUtils.UTF8);
+    }
+
+    @Override
+    public byte[] serialize(Object object) {
+        if(object==null){
+            return new byte[0];
+        }
+
+        try {
+            return JSON.toJSONBytes(object, SerializerFeature.WriteClassName);
+        } catch (Exception ex) {
+            throw new SerializationException("Could not serialize: " + ex.getMessage(), ex);
+        }
+    }
+
+    @Override
+    public byte[] serialize(String data) {
+        if(data==null||data.length()==0){
+            return new byte[0];
+        }
+
+        return data.getBytes(Charset.forName("utf-8"));
+    }
+}
