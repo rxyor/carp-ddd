4ceb3e7e01f63650bf10d5ad8fb2afa93f95b25d
刘阳 <liuyang03@souche.com>
2019-12-30 11:09:25 +0800

    @chore:修改配置

--- carp-auth/carp-auth-start/src/main/java/com/github/rxyor/carp/auth/AuthApplication.java
+++ carp-auth/carp-auth-start/src/main/java/com/github/rxyor/carp/auth/AuthApplication.java
@@ -1,5 +1,8 @@
 package com.github.rxyor.carp.auth;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *<p>
  *
@@ -9,6 +12,10 @@ package com.github.rxyor.carp.auth;
  * @date 2019/12/30 周一 09:39:00
  * @since 1.0.0
  */
+@SpringBootApplication
 public class AuthApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class);
+    }
 }
