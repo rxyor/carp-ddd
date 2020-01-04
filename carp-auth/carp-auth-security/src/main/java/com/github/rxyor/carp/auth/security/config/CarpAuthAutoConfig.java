1c31c7deee56e3096ee766b135494840f554a8ea
刘阳 <liuyang03@souche.com>
2020-01-04 12:10:43 +0800

    @chore:移动配置

--- carp-auth/carp-auth-security/src/main/java/com/github/rxyor/carp/auth/security/config/CarpAuthAutoConfig.java
+++ carp-auth/carp-auth-security/src/main/java/com/github/rxyor/carp/auth/security/config/CarpAuthAutoConfig.java
@@ -1,8 +1,25 @@
 package com.github.rxyor.carp.auth.security.config;
 
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.rxyor.carp.auth.security.support.oauth2.common.DefaultOauth2RefreshTokenSerializer;
+import com.github.rxyor.carp.auth.security.support.oauth2.provider.CarpAccessDeniedHandler;
+import com.github.rxyor.carp.auth.security.support.oauth2.provider.CarpClientDetailsService;
+import com.github.rxyor.carp.auth.security.support.oauth2.provider.CarpWebResponseExceptionTranslator;
+import com.github.rxyor.carp.auth.security.support.security.core.CarpUserDetailsService;
+import com.github.rxyor.carp.auth.security.support.security.web.ResourceAuthExceptionEntryPoint;
+import com.github.rxyor.carp.ums.api.feign.user.UserFeignService;
+import com.github.rxyor.common.support.util.RedisKeyBuilder;
+import javax.sql.DataSource;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.common.DefaultOAuth2RefreshToken;
 
 /**
  *<p>
@@ -19,4 +36,54 @@ import org.springframework.context.annotation.Configuration;
     value = {CarpAuthClientProperties.class, CarpAuthResourceProperties.class})
 public class CarpAuthAutoConfig {
 
+
+    protected static void init() {
+        //自定义oauth2序列化：DefaultOAuth2RefreshToken 没有setValue方法，会导致JSON序列化为null
+        ParserConfig.getGlobalInstance().putDeserializer(
+            DefaultOAuth2RefreshToken.class, new DefaultOauth2RefreshTokenSerializer());
+    }
+
+    static {
+        init();
+    }
+
+    @Value("${spring.application.name}")
+    private String appName;
+
+    @Bean
+    @Primary
+    public CarpUserDetailsService carpUserDetailsService(
+        UserFeignService userFeignService,
+        CarpAuthClientProperties carpAuthClientProperties,
+        RedissonClient redissonClient) {
+        return new CarpUserDetailsService(carpAuthClientProperties, userFeignService, redissonClient);
+    }
+
+    @Bean
+    @Primary
+    public CarpClientDetailsService carpClientDetailsService(DataSource dataSource) {
+        return new CarpClientDetailsService(dataSource);
+    }
+
+    @Bean
+    @Primary
+    public ResourceAuthExceptionEntryPoint resourceAuthExceptionEntryPoint(ObjectMapper objectMapper) {
+        return new ResourceAuthExceptionEntryPoint(objectMapper);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public RedisKeyBuilder redisKeyBuilder() {
+        return new RedisKeyBuilder(appName, "::");
+    }
+
+    @Bean
+    public CarpWebResponseExceptionTranslator webResponseExceptionTranslator() {
+        return new CarpWebResponseExceptionTranslator();
+    }
+
+    @Bean
+    public CarpAccessDeniedHandler accessDeniedHandler(ObjectMapper objectMapper) {
+        return new CarpAccessDeniedHandler(objectMapper);
+    }
 }
