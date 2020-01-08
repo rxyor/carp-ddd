b2bcf4a384daa72200f389cda6bd1c8e5eafc7c4
刘阳 <liuyang03@souche.com>
2020-01-09 00:43:11 +0800

    nacos配置

--- carp-auth/carp-auth-start/src/main/java/com/github/rxyor/carp/auth/AuthApplication.java
+++ carp-auth/carp-auth-start/src/main/java/com/github/rxyor/carp/auth/AuthApplication.java
@@ -1,8 +1,8 @@
 package com.github.rxyor.carp.auth;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -16,11 +16,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @since 1.0.0
  */
 @EnableCaching(proxyTargetClass = true)
-@SpringBootApplication
-@EnableDiscoveryClient
 @EnableFeignClients(basePackages = {
     "com.github.rxyor.carp.ums.api"
 })
+@EnableDiscoveryClient
+@SpringCloudApplication
 public class AuthApplication {
 
     public static void main(String[] args) {
