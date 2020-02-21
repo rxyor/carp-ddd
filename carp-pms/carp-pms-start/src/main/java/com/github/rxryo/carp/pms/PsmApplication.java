dde9a28c049b603c8bdfcfe3e59ed3c7f9691b8f
错误 <liuyangrxy@163.com>
2020-02-21 19:07:29 +0800

    docker部署 增加跨域配置

---
+++ carp-pms/carp-pms-start/src/main/java/com/github/rxryo/carp/pms/PsmApplication.java
@@ -0,0 +1,17 @@
+package com.github.rxryo.carp.pms;
+
+/**
+ *<p>
+ *
+ *</p>
+ *
+ * @author liuyang
+ * @date 2020/2/21 周五 17:00:00
+ * @since 1.0.0
+ */
+public class PsmApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
