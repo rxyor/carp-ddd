90d5bc1279e29d322034968ca3ca85f7af948e1e
错误 <liuyangrxy@163.com>
2020-02-23 16:10:22 +0800

    修改脚本

--- carp-gateway/setup.sh
+++ carp-gateway/setup.sh
@@ -22,6 +22,6 @@ docker run\
 # 查看日志
 docker logs carp-gateway;
 #删除build过程中产生的镜像    #docker image prune -a -f
-docker rmi $(docker images | grep carp-gateway -f "dangling=true" -q) || exit 1;
+docker rmi $(docker images | grep carp-gateway -f "dangling=true" -q) > /dev/null 2>&1
 # 对空间进行自动清理
 #docker system prune -a -f
